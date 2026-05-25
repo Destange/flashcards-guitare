d430a3835202cafb5b42115dd1b7972ba420e1c0
Destange <juliendestange@gmail.com>
2026-05-25 04:09:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'guitare-v1.4';
+var CACHE = 'guitare-v1.5';
 var ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
